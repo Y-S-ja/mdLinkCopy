faa30df93ae94bfe9fda97fcd22083ee262d9142
Y-S-ja <ys40980526@gmail.com>
2026-03-11 16:39:51 +0900

    clean: コメント

--- options.js
+++ options.js
@@ -1,6 +1,5 @@
 // 設定を読み込む
 function restoreOptions() {
-    // インストール時に初期化されているため、単純に取得するだけでOK
     chrome.storage.sync.get('notice-duration', (items) => {
         if (items['notice-duration'] !== undefined) {
             document.getElementById('notice-duration').value = items['notice-duration'];
@@ -8,17 +7,16 @@ function restoreOptions() {
     });
 }
 
-// 設定を自動保存する
+// 設定を保存する
 function saveOptions() {
     const noticeDuration = parseInt(document.getElementById('notice-duration').value, 10);
 
-    // バリデーション（数値以外や極端な値を防ぐ）
     if (isNaN(noticeDuration) || noticeDuration < 0) return;
 
     chrome.storage.sync.set({
         'notice-duration': noticeDuration
     }, () => {
-        // 保存完了をふわっと出す
+        // 保存完了を通知
         const status = document.getElementById('status');
         status.classList.add('show');
         setTimeout(() => {
